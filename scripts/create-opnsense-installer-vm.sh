b4ded53ef2926279771e73eacd0c7357336e36bb
lasubp <lasubp@gmailcom>
2026-03-20 17:46:43 +0200

    Refactor OPNsense VM creation script to support additional parameters and improve flexibility

--- scripts/create-opnsense-installer-vm.sh
+++ scripts/create-opnsense-installer-vm.sh
@@ -9,12 +9,11 @@ ISO_STORE="local"
 ISO_FILE="OPNsense-installer.iso"
 MEMORY="4096"
 CORES="2"
+DISK_SIZE="32"
+BIOS="seabios"
+NETWORK_MODEL="virtio"
 WAN_BRIDGE="vmbr1"
-LAN1_BRIDGE="vmbr2"
-LAN2_BRIDGE="vmbr3"
-LAN3_BRIDGE="vmbr4"
-LAN4_BRIDGE="vmbr5"
-LAN5_BRIDGE="vmbr6"
+LAN_BRIDGES=(vmbr2 vmbr3 vmbr4 vmbr5 vmbr6)
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -24,14 +23,36 @@ while [[ $# -gt 0 ]]; do
     --storage) STORAGE="$2"; shift 2 ;;
     --iso-store) ISO_STORE="$2"; shift 2 ;;
     --iso-file) ISO_FILE="$2"; shift 2 ;;
+    --memory) MEMORY="$2"; shift 2 ;;
+    --cores) CORES="$2"; shift 2 ;;
+    --disk-size) DISK_SIZE="$2"; shift 2 ;;
+    --bios) BIOS="$2"; shift 2 ;;
+    --network-model) NETWORK_MODEL="$2"; shift 2 ;;
+    --wan-bridge) WAN_BRIDGE="$2"; shift 2 ;;
+    --lan-bridges) IFS=',' read -r -a LAN_BRIDGES <<< "$2"; shift 2 ;;
     *) echo "Unknown arg: $1" >&2; exit 1 ;;
   esac
 done
 
 if [[ -z "$VMID" || -z "$NODE" ]]; then
-  echo "Usage: $0 --vmid <id> --node <node> [--name ...] [--storage ...] [--iso-store ...] [--iso-file ...]" >&2
+  echo "Usage: $0 --vmid <id> --node <node> [--name ...] [--storage ...] [--iso-store ...] [--iso-file ...] [--memory ...] [--cores ...] [--disk-size ...] [--bios ...] [--network-model ...] [--wan-bridge ...] [--lan-bridges bridge1,bridge2,...]" >&2
   exit 1
 fi
+
+if [[ ${#LAN_BRIDGES[@]} -eq 0 ]]; then
+  echo "Provide at least one LAN bridge with --lan-bridges." >&2
+  exit 1
+fi
+
+if ! pvesm path "${ISO_STORE}:iso/${ISO_FILE}" >/dev/null 2>&1; then
+  echo "ISO not found in Proxmox storage: ${ISO_STORE}:iso/${ISO_FILE}" >&2
+  exit 1
+fi
+
+NET_ARGS=("--net0" "${NETWORK_MODEL},bridge=${WAN_BRIDGE}")
+for idx in "${!LAN_BRIDGES[@]}"; do
+  NET_ARGS+=("--net$((idx + 1))" "${NETWORK_MODEL},bridge=${LAN_BRIDGES[$idx]}")
+done
 
 qm destroy "$VMID" --purge 1 >/dev/null 2>&1 || true
 qm create "$VMID" \
@@ -41,17 +62,12 @@ qm create "$VMID" \
   --cores "$CORES" \
   --scsihw virtio-scsi-pci \
   --ostype l26 \
-  --bios seabios \
+  --bios "$BIOS" \
   --serial0 socket \
   --vga serial0 \
-  --net0 virtio,bridge="$WAN_BRIDGE" \
-  --net1 virtio,bridge="$LAN1_BRIDGE" \
-  --net2 virtio,bridge="$LAN2_BRIDGE" \
-  --net3 virtio,bridge="$LAN3_BRIDGE" \
-  --net4 virtio,bridge="$LAN4_BRIDGE" \
-  --net5 virtio,bridge="$LAN5_BRIDGE"
+  "${NET_ARGS[@]}"
 
-qm set "$VMID" --scsi0 "$STORAGE":32
+qm set "$VMID" --scsi0 "$STORAGE":"$DISK_SIZE"
 qm set "$VMID" --ide2 "$ISO_STORE":iso/"$ISO_FILE",media=cdrom
 qm set "$VMID" --boot order=ide2
 
