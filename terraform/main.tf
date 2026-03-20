8a7d084db6fe1838c3f501f5f9bd3b976c4143a9
lasubp <lasubp@gmailcom>
2026-03-20 17:48:06 +0200

    Refactor main.tf to enhance VM configuration with dynamic network indexing and improved variable usage

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
 locals {
-  ssh_public_key = trimspace(file(var.ssh_public_key_file))
+  ssh_public_key       = trimspace(file(pathexpand(var.ssh_public_key_file)))
+  sorted_network_names = sort(keys(var.internal_networks))
+  network_index_by_name = {
+    for idx, net_name in local.sorted_network_names : net_name => idx
+  }
 
   ubuntu_vms = merge([
     for net_name, net in var.internal_networks : {
@@ -9,56 +13,47 @@ locals {
         ip      = ip
         cidr    = split("/", net.subnet)[1]
         gateway = net.gateway
-        vmid    = 2100 + (index(keys(var.internal_networks), net_name) * 10) + idx
+        vmid    = var.ubuntu_vmid_base + (local.network_index_by_name[net_name] * var.ubuntu_vmid_stride) + idx
       }
     }
   ]...)
-
-  sorted_network_names = sort(keys(var.internal_networks))
 }
 
 resource "proxmox_vm_qemu" "opnsense" {
-  name        = "lab-opnsense"
+  name        = var.opnsense_vm_name
   target_node = var.target_node
   clone       = var.opnsense_template_name
   full_clone  = true
 
-  vmid        = 2000
-  onboot      = true
-  os_type     = "other"
-  scsihw      = "virtio-scsi-pci"
-  boot        = "order=scsi0"
-  agent       = 0
-  cores       = var.opnsense_cores
-  memory      = var.opnsense_memory
-  balloon     = 0
-  ciupgrade   = false
+  vmid    = var.opnsense_vmid
+  onboot  = true
+  os_type = "other"
+  scsihw  = "virtio-scsi-pci"
+  boot    = "order=scsi0"
+  agent   = 0
+  cores   = var.opnsense_cores
+  memory  = var.opnsense_memory
+  balloon = 0
 
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          storage = var.clone_storage
-          size    = var.opnsense_disk_size
-        }
-      }
-    }
+  disk {
+    slot    = 0
+    type    = "scsi"
+    storage = var.clone_storage
+    size    = var.opnsense_disk_size
   }
 
   network {
-    id       = 0
-    model    = "virtio"
+    model    = var.opnsense_network_model
     bridge   = var.wan_bridge
-    firewall = true
+    firewall = var.enable_proxmox_firewall
   }
 
   dynamic "network" {
     for_each = local.sorted_network_names
     content {
-      id       = network.key + 1
-      model    = "virtio"
+      model    = var.opnsense_network_model
       bridge   = var.internal_networks[network.value].bridge
-      firewall = true
+      firewall = var.enable_proxmox_firewall
     }
   }
 }
@@ -70,39 +65,33 @@ resource "proxmox_vm_qemu" "ubuntu" {
   clone       = var.ubuntu_template_name
   full_clone  = true
 
-  vmid        = each.value.vmid
-  onboot      = true
-  os_type     = "cloud-init"
-  agent       = 1
-  qemu_os     = "l26"
-  scsihw      = "virtio-scsi-pci"
-  boot        = "order=scsi0"
-  ciupgrade   = true
-  ciuser      = var.vm_username
-  sshkeys     = local.ssh_public_key
-  ipconfig0   = "ip=${each.value.ip}/${each.value.cidr},gw=${each.value.gateway}"
-  nameserver  = join(" ", var.dns_servers)
+  vmid         = each.value.vmid
+  onboot       = true
+  os_type      = "cloud-init"
+  agent        = 1
+  qemu_os      = "l26"
+  scsihw       = "virtio-scsi-pci"
+  boot         = "order=scsi0"
+  ciuser       = var.vm_username
+  sshkeys      = local.ssh_public_key
+  ipconfig0    = "ip=${each.value.ip}/${each.value.cidr},gw=${each.value.gateway}"
+  nameserver   = join(" ", var.dns_servers)
   searchdomain = var.searchdomain
 
-  cores       = var.ubuntu_cores
-  memory      = var.ubuntu_memory
-  balloon     = 0
+  cores   = var.ubuntu_cores
+  memory  = var.ubuntu_memory
+  balloon = 0
 
-  disks {
-    scsi {
-      scsi0 {
-        disk {
-          storage = var.clone_storage
-          size    = var.ubuntu_disk_size
-        }
-      }
-    }
+  disk {
+    slot    = 0
+    type    = "scsi"
+    storage = var.clone_storage
+    size    = var.ubuntu_disk_size
   }
 
   network {
-    id       = 0
-    model    = "virtio"
+    model    = var.ubuntu_network_model
     bridge   = each.value.bridge
-    firewall = true
+    firewall = var.enable_proxmox_firewall
   }
 }
